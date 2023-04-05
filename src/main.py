adbd7dfed92d3d382ba176d3956af65db54a6ad2
Tomas Santana <tomas.30604530@uru.edu>
2023-04-05 11:23:13 -0400

    Added better language support. Changed language change flow.

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ responses = {
         "too_long": "El archivo es demasiado largo\. La duracion maxima es de 20 segundos",
         "error": "Ha ocurrido un error\. Por favor, intenta de nuevo mas tarde",
         "lang_change": "El idioma ha sido cambiado a Español",
-        "lang_change_prompt": "Escribe el idioma al que quieres cambiar.\n\nIdiomas disponibles:\n\n/lang *English* - En-US\n/lang *Español* - Es-ES",
+        "lang_change_prompt": "Idiomas disponibles:\n\nTo change the language to english, send\n/lang EN\n\nPara cambiar el idioma a español, send\n /lang ES\n\n Для смены языка на русский, отправьте\n/lang RU\n\nPer cambiare lingua in italiano, invia\n /lang IT",
         "lang_change_error": "El idioma que ingresó no esta disponible"
     },
     "En-US": {
@@ -32,12 +32,32 @@ responses = {
         "too_long": "The file is too long\. The maximum duration is 20 seconds",
         "error": "An error has occurred\. Please try again later",
         "lang_change": "The language has been changed to English",
-        "lang_change_prompt": "Write the language you want to change to\.\n\nAvailable languages:\n\n/lang *English* - En-US\n/lang *Español* - Es-ES",
+        "lang_change_prompt": "Available languages:\n\nTo change the language to english, send\n/lang EN\n\nPara cambiar el idioma a español, send\n /lang ES\n\n Для смены языка на русский, отправьте\n/lang RU\n\nPer cambiare lingua in italiano, invia\n /lang IT",
         "lang_change_error": "The language you entered is not available"
+    },
+    "ru-RU": {
+        "start": "**Голос в текст**\n\nОтправьте голосовое сообщение, чтобы оно было преобразовано в текст\.\n\nВ настоящее время файлы должны иметь максимальную продолжительность 20 секунд\.\n\nЕсли вы хотите изменить язык, используйте команду /lang",
+        "processing": "Транскрипция в процессе",
+        "too_long": "Файл слишком длинный\. Максимальная продолжительность 20 секунд",
+        "error": "Произошла ошибка\. Пожалуйста, повторите попытку позже",
+        "lang_change": "Язык изменен на русский",
+        "lang_change_prompt": "Доступные языки:\n\nTo change the language to english, send\n/lang EN\n\nPara cambiar el idioma a español, send\n /lang ES\n\n Для смены языка на русский, отправьте\n/lang RU\n\nPer cambiare lingua in italiano, invia\n /lang IT",
+        "lang_change_error": "Введенный язык недоступен"
+    },
+    "it-IT": {
+        "start": "**Voce a testo**\n\nInvia un messaggio vocale da trascrivere in testo\.\n\nAttualmente, i file devono avere una durata massima di 20 secondi\.\n\nSe vuoi cambiare la lingua, usa il comando /lang",
+        "processing": "La trascrizione è in corso",
+        "too_long": "Il file è troppo lungo\. La durata massima è di 20 secondi",
+        "error": "Si è verificato un errore\. Riprova più tardi",
+        "lang_change": "La lingua è stata cambiata in italiano",
+        "lang_change_prompt": "Lingue disponibili:\n\nTo change the language to english, send\n/lang EN\n\nPara cambiar el idioma a español, send\n /lang ES\n\n Для смены языка на русский, отправьте\n/lang RU\n\nPer cambiare lingua in italiano, invia\n /lang IT",
     }
+
 }
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    language = update.effective_user.language_code
+    set_language(language)
     await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]["start"], parse_mode='MarkdownV2')
 
 async def get_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -51,26 +71,45 @@ async def get_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
 
     converter.ogg_to_wav('audio.ogg')
-    text = converter.get_text_from_voice('converted.wav')
+    text = converter.get_text_from_voice('converted.wav', language=lang)
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Transcripcion:\n\n {text}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Transcripcion:\n\n{text}")
 
     os.remove('audio.ogg')
     os.remove('converted.wav')
 
-async def set_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def user_set_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global lang
     if len(context.args) == 0:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change_prompt'])
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change_prompt'], parse_mode='MarkdownV2')
         return
     language = " ".join(context.args)
-    if language == "English":
+    if language == "EN":
         lang = "En-US"
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change'])
-    elif language == "Español":
+    elif language == "ES":
         lang = "Es-ES"
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change'])
+    elif language == "RU":
+        lang = "ru-RU"
+    elif language == "IT":
+        lang = "it-IT"
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change_error'])
+        return
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=responses[lang]['lang_change'])
 
+
+def set_language(language):
+    global lang
+    if language == "en":
+        lang = "En-US"
+    elif language == "es":
+        lang = "Es-ES"
+    elif language == "ru":
+        lang = "ru-RU"
+    elif language == "it":
+        lang = "it-IT"
+    else:
+        lang = "En-US"
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(API_KEY).build()
@@ -81,7 +120,7 @@ if __name__ == '__main__':
     update_handler = MessageHandler(filters.VOICE & (~filters.COMMAND), get_voice_message)
     application.add_handler(update_handler)
 
-    language_handler = CommandHandler('lang', set_language)
+    language_handler = CommandHandler('lang', user_set_language)
     application.add_handler(language_handler)
 
     application.run_polling()
